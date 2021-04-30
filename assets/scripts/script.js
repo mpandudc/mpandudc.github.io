c503ec858156b3c0cddb18d5cccfc3909bda105c
Muhammad Pandu Dwi Cahyo <mpandudc@gmail.com>
2021-04-30 21:15:04 +0700

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  var entries = [
+    { label: 'Python' },
+    { label: 'Machine Learning' },
+    { label: 'Deep Learning' },
+    { label: 'R' },
+    { label: 'C/C++' },
+    { label: 'Javascript' },
+    { label: 'HTML' },
+    { label: 'SQL' },
+    { label: 'Regression' },
+    { label: 'Classification' },
+    { label: 'Data Visualization' },
+    { label: 'CSS' },
+    { label: 'Model Deployment' },
+    { label: 'Data Scraping' },
+    { label: 'Java' },
+    { label: 'PHP' },
+    { label: 'Design' },
+  ];
+
+  var settings = {
+    entries: entries,
+    width: 680,
+    height: 680,
+    radius: '70%',
+    radiusMin: 75,
+    bgDraw: true,
+    bgColor: '#fff',
+    opacityOver: 2.0,
+    opacityOut: 0.05,
+    opacitySpeed: 6,
+    fov: 800,
+    speed: 0.4,
+    fontSize: '20',
+    fontColor: 'black',
+    fontWeight: 'bold', //bold
+    fontStyle: 'normal', //italic
+    fontStretch: 'normal', //wider, narrower, ultra-condensed, extra-condensed, condensed, semi-condensed, semi-expanded, expanded, extra-expanded, ultra-expanded
+    fontToUpperCase: true,
+  };
+
+  //var svg3DTagCloud = new SVG3DTagCloud( document.getElementById( 'holder'  ), settings );
+  $('#holder').svg3DTagCloud(settings);
+});
